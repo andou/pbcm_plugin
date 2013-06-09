57a46d3e5427bb8f2ecd7997bfcf1e44880d27fe
andou <antonio.pastorino@gmail.com>
2013-06-09 12:06:03 +0200

    Eliminazione campo creation year dal prodotto

--- classes/PcdmProduct.php
+++ classes/PcdmProduct.php
@@ -54,46 +54,32 @@ class PcdmProduct {
     }
 
     public function defineFields() {
-        add_meta_box('creation-year-id', 'Creation Year', array(&$this, 'defineCreationYear'), self::TYPE_IDENTIFIER, 'side', 'low');
+//        add_meta_box('creation-year-id', 'Creation Year', array(&$this, 'defineCreationYear'), self::TYPE_IDENTIFIER, 'side', 'low');
     }
 
-    public function defineCreationYear() {
-        global $post;
-        $custom = get_post_custom($post->ID);
-        $creation_year = $custom["creation_year"][0];
-        ?>  
-        <label for="creation_year_text">Creation Year</label>  
-        <input type="text" name="creation_year" value="<?= $creation_year ?>" id="creation_year" />  
-        <?php
-    }
 
-    public function saveDetails() {
-        global $post;
-        update_post_meta($post->ID, "creation_year", $_POST["creation_year"]);
-    }
 
     public function save($product_id) {
-
-        // Bail if we're doing an auto save  
-        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
-            return;
-
-        // if our current user can't edit this post, bail  
-        if (!current_user_can('edit_post'))
-            return;
-
-        global $post;
-        $post_id = $post->ID;
-
-        // now we can actually save the data  
-        $allowed = array(
-            'a' => array(// on allow a tags  
-                'href' => array() // and those anchors can only have href attribute  
-            )
-        );
-
-        if (isset($_POST['creation_year']))
-            update_post_meta($post_id, 'creation_year', wp_kses($_POST['creation_year'], $allowed));
+//        // Bail if we're doing an auto save  
+//        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
+//            return;
+//
+//        // if our current user can't edit this post, bail  
+//        if (!current_user_can('edit_post'))
+//            return;
+//
+//        global $post;
+//        $post_id = $post->ID;
+//
+//        // now we can actually save the data  
+//        $allowed = array(
+//            'a' => array(// on allow a tags  
+//                'href' => array() // and those anchors can only have href attribute  
+//            )
+//        );
+//
+//        if (isset($_POST['creation_year']))
+//            update_post_meta($post_id, 'creation_year', wp_kses($_POST['creation_year'], $allowed));
     }
 
 }
